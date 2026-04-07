86731c029cb293fcf38bbe0965aa92ff3a61a97d
minbang <idabc1@naver.com>
2026-04-07 19:03:49 +0900

    feat: initial Spring Boot project setup

---
+++ gitflow/src/main/java/com/example/gitflow/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.gitflow;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GitflowApplication.class);
+    }
+
+}
